f499b8051b5ba545d2f960e67b6f9d016e1a9843
Mário Ribeiro <mail@marioribeiro.com>
2017-04-19 23:32:14 +0100

    Add solution

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -5,7 +5,25 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+#class String
+#  def every_other_char
+#    return_string = ""
+#    split.map.each_with_index do |word, index|
+#      return_string << char if index.even?
+#    end
+#    return_string.to_s
+#  end
+#end
+# This is returning every other word.. with no spaces... checked the solution and learned the each_char ** BOOM **
+# I was kinda doing the same thing here.. but not quite what was needed
+
+
 class String
   def every_other_char
+    to_return = ''
+    each_char.with_index do |char, index|
+      to_return << char if index.even?
+    end
+    to_return
   end
 end
